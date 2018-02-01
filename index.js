51e5710938e5fc70652aadc49bb9f067c668744a
Jerry Lee <jwl62@georgetown.edu>
2018-02-01 04:44:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function whisper(string){
   return string.toLowercase();
 }
 
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
